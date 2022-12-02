90a32ef5cb3ac351434e2ab0363efa58bfe608ce
EastPersiaLtd <eastpersialtd@gmail.com>
2022-12-02 21:58:49 +0900

    First Patch

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-This is a Web Scraper Script for Python;
-Just, execute arcalive.py file
+# Basic Information
+
+## English(GB)
+This is the Arcalive Web Scraper for Python.
+Just, execute arcalive.py file.
+Currently, Only Korean version available.
+Google Colaboratory Friendly.
 example) python3 arcalive.py
 
+Planned to add automatic Wordcloud generation in the near future.
+
 -EastPersiaLtd
+
+## Korean(KR)
+Python용 아카라이브 웹 스크래퍼입니다.
+arcalive.py로 실행하면 됩니다.
+현재 한국어 버전만 존재합니다.
+구글 코러보러트리 지원.
+예시) python3 arcalive.py
+
+추후 워드클라우드 자동화 추가 예정.
+
+-EastPersiaLtd
+
+# Contact
+eastpersialtd@gmail.com
--- arcalive.py
+++ arcalive.py
@@ -1,3 +1,5 @@
+!pip install bs4 pandas
+
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
@@ -5,6 +7,10 @@ import time
 post_title=[]
 post_author=[]
 #####
+print('아카라이브 웹 스크래퍼입니다.\n주요 채널 목록은 아래와 같습니다.\n')
+print('원신/genshin:    6400p/month\n블루아카이브/bluearchive:    4500p/month\n붕괴3rd/hk3rd:    2400p/month\n카운터사이드/counterside:    2000p/month')
+print('던전앤파이터/dunfa:    1800p/month\n명일방주/arknights:    1600p/month\n라스트오리진/lastorigin:    900p/month\n아즈렌/azurlane:    750p/month')
+print('프린세스 커넥트 Re:Dive/prcn:    300p/month\n')
 channel=str(input('채널의 주소를 영문으로 입력하세요; https://arca.live/b/(채널 주소): '))
 n=int(input('크롤링할 페이지의 범위를 입력하세요; 최근 n 페이지만큼-> n:'))
 creation_date=str(time.strftime('%Y-%m-%d-%H-%M', time.localtime(time.time())))
@@ -17,7 +23,6 @@ for i in range(n):
     #####
     title=soup.select('span.title')
     author=soup.select('span.user-info')
-    #time=soup.select('span.vcol.col-time')
     #####
     for j in title:
       post_title.append(j.get_text())
@@ -27,4 +32,6 @@ for i in range(n):
 post_list = list(zip(post_title, post_author))
 col=['post_title', 'post_author']
 df=pd.DataFrame(post_list, columns=col)
+df=df.replace(r'\n',  ' ', regex=True)
 df.to_csv(f'arcalive_{channel}_page_{n}_{creation_date}.csv', index=False, encoding="utf-8-sig")
+print(f'아카라이브 {channel} 채널의 최근 {n} 페이지 데이터 수집이 완료되었습니다.\n다운로드된 파일은 폴더를 열어 확인해주세요.\n제작자: EastPersiaLtd')
